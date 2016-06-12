a2d9a9cb67d72525f6159bea03fffc06091d7a8a
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-06-11 23:48:28 -0300

    refactor and fix tests opetions with no login

--- 2-tdd-desenvolviment-de-software-guiado-por-testes/w3/caixa-eletronico/testes/CaixaEletronicoOperacoesSemLogarTest.java
+++ 2-tdd-desenvolviment-de-software-guiado-por-testes/w3/caixa-eletronico/testes/CaixaEletronicoOperacoesSemLogarTest.java
@@ -3,21 +3,16 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class CaixaEletronicoOperacoesSemLogarTest {
     private CaixaEletronico caixa;
-    private HardwareMock hardwareMock;
     private ServicoRemotoMock servicoRemotoMock;
-    private ContaCorrente contaCorrente;
 
     @Before
     public void setUp() {
-        hardwareMock = new HardwareMock();
         servicoRemotoMock = new ServicoRemotoMock();
-        caixa = new CaixaEletronico(hardwareMock, servicoRemotoMock);
-        contaCorrente = new ContaCorrente("1", "senhasecreta", new BigDecimal("100.00"));
+        caixa = new CaixaEletronico(new HardwareMock(), servicoRemotoMock);
     }
 
     @Test(expected = UsuarioNaoLogadoException.class)
@@ -27,7 +22,7 @@ public class CaixaEletronicoOperacoesSemLogarTest {
 
     @Test(expected = UsuarioNaoLogadoException.class)
     public void sacarSemLogar() throws UsuarioNaoLogadoException, ProblemaHardwareException {
-        caixa.sacar(new BigDecimal("100.01"));
+        caixa.sacar(new BigDecimal("10.00"));
 
         assertFalse(servicoRemotoMock.chamouMetodoPersistirConta());
     }
@@ -36,7 +31,6 @@ public class CaixaEletronicoOperacoesSemLogarTest {
     public void depositarSemLogar() throws UsuarioNaoLogadoException, ProblemaHardwareException {
         caixa.depositar(new BigDecimal("10.00"));
 
-        assertEquals(new BigDecimal("100.00"), contaCorrente.saldo());
         assertFalse(servicoRemotoMock.chamouMetodoPersistirConta());
     }
 }
