e2e9e0eba2365bc64bd04a2b91536b8769be41fd
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-09-20 21:26:55 -0300

    change db image tests and change tests

--- 3-desenvolvimento-agil-com-java-avancado/w4/forum/testes/daos/UsuarioDaoTest.java
+++ 3-desenvolvimento-agil-com-java-avancado/w4/forum/testes/daos/UsuarioDaoTest.java
@@ -21,9 +21,9 @@ public class UsuarioDaoTest {
 
     @Before
     public void database() throws Exception {
-        jdt = new JdbcDatabaseTester("org.postgresql.Driver", "jdbc:postgresql://localhost/coursera", "postgres", "admin");
+        jdt = new JdbcDatabaseTester("org.postgresql.Driver", "jdbc:postgresql://localhost/forum", "postgres", "admin");
         loader = new FlatXmlDataFileLoader();
-        IDataSet dataSet = loader.load("/daos/database-images/usuarios-inicio.xml");
+        IDataSet dataSet = loader.load("/daos/database-images/inicio.xml");
         jdt.setDataSet(dataSet);
         jdt.onSetup();
         dao = new UsuarioDao();
@@ -31,41 +31,43 @@ public class UsuarioDaoTest {
 
     @Test
     public void testeRecuperarPorLogin() {
-        Usuario usuarioEsperado = new Usuario("maria", "maria@gmail.com", "Maria de Souza", "maria123", 30);
+        Usuario usuarioEsperado = new Usuario("mario", "mario@nintendo.com", "It's me, Mario!", "mario", 13);
 
-        Usuario resposta = dao.recuperar("maria");
+        Usuario resposta = dao.recuperar("mario");
 
         assertEquals(usuarioEsperado, resposta);
     }
 
     @Test
     public void testeRanking() {
-        Usuario usuarioEsperadoPrimeiro = new Usuario("maria", "maria@gmail.com", "Maria de Souza", "maria123", 30);
-        Usuario usuarioEsperadoSegundo = new Usuario("luquinhas", "lucas@hotmail.com", "Lucas Pereira", "lucca", 20);
+        Usuario usuarioEsperadoPrimeiro = new Usuario("mario", "mario@nintendo.com", "It's me, Mario!", "mario", 13);
+        Usuario usuarioEsperadoSegundo = new Usuario("luigi", "luigi@naointendo.com", "Luigi", "marioverde", 10);
+        Usuario usuarioEsperadoTerceiro = new Usuario("peach", "peach@nintendo.com", "Peach", "socorro", 6);
 
         List<Usuario> resultado = dao.ranking();
 
-        assertEquals(2, resultado.size());
+        assertEquals(3, resultado.size());
         assertEquals(usuarioEsperadoPrimeiro, resultado.get(0));
         assertEquals(usuarioEsperadoSegundo, resultado.get(1));
+        assertEquals(usuarioEsperadoTerceiro, resultado.get(2));
     }
 
     @Test
     public void testeInsercao() throws Exception {
-        dao.inserir(new Usuario("pedro", "pedro@gmail.com", "Pedro Silva", "pedro123", 10));
+        dao.inserir(new Usuario("goomba", "goomba@nintendo.com", "Goomba", "", 0));
 
         ITable tabelaCorrente = jdt.getConnection().createDataSet().getTable("usuario");
-        ITable tabelaEsperada = loader.load("/daos/database-images/usuarios-apos-insercao.xml").getTable("usuario");
+        ITable tabelaEsperada = loader.load("/daos/database-images/apos-insercao-usuario.xml").getTable("usuario");
 
         Assertion.assertEquals(tabelaEsperada, tabelaCorrente);
     }
 
     @Test
     public void testeAdicionaPontos() throws Exception {
-        dao.adicionarPontos("maria", 5);
+        dao.adicionarPontos("mario", 5);
 
         ITable tabelaCorrente = jdt.getConnection().createDataSet().getTable("usuario");
-        ITable tabelaEsperada= loader.load("/daos/database-images/usuarios-apos-adicao-de-pontos.xml").getTable("usuario");
+        ITable tabelaEsperada= loader.load("/daos/database-images/apos-adicao-de-pontos.xml").getTable("usuario");
 
         Assertion.assertEquals(tabelaCorrente, tabelaEsperada);
     }
